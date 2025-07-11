69d7989a910633db6837d3e940fa2edcba896aae
Michel SAN <msan@werefrog.net>
2025-07-11 23:51:21 +0200

    Adds text node with type enum
    Adds a TextNode class to represent text with different types (plain, bold, italic, etc.).
    Also adds TextType enum to define the available text types.
    
    This facilitates structured handling of text formatting within the application.

---
+++ src/textnode.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.text}, {self.text_type.value}, {self.url})"
+
+
+def main():
+    node = TextNode("This is some anchor text", text_type=TextType.LINK, url="https://www.boot.dev")
+    print(node)
+
+
+if __name__ == '__main__':
+    main()
