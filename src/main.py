f23dcaf7bd10e86019e413d92243a6b9273d6bf2
Michel SAN <msan@werefrog.net>
2025-07-11 23:51:26 +0200

    Introduces text node functionality
    Adds a text node class with support for different text types, such as links, and demonstrates its usage.
    
    Removes the initial placeholder code.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is some anchor text", text_type=TextType.LINK, url="https://www.boot.dev")
+    print(node)
+
+
+if __name__ == '__main__':
+    main()
